562b2be1279394e3925542de96020dbc04511b1d
Claire Lally <clairelally08@gmail.com>
2020-06-09 22:35:42 +0000

    rendering the dashboard if a user is logged in

--- main/views.py
+++ main/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+    if request.user.is_authenticated:
+        return render(request, 'main/dashboard.html')
+
     return render(request, 'main/index.html')
 
 def dashboard(request):
