a0c34dc72d8e581609976422fdb6fa8f6fa8f8f2
Claire Lally <clairelally08@gmail.com>
2020-06-10 22:50:58 +0000

    view for shop

--- main/views.py
+++ main/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
 def index(request):
     if request.user.is_authenticated:
         return render(request, 'main/dashboard.html')
 
     return render(request, 'main/index.html')
 
+
 def dashboard(request):
     return render(request, 'main/dashboard.html')
+
+
+def shop(request):
+    return render(request, 'main/shop.html')
