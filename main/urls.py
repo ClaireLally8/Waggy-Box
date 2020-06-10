db13e54689bc2dae44324d6d8c329a4b5c000c76
Claire Lally <clairelally08@gmail.com>
2020-06-10 22:50:47 +0000

    URL for shop

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('dashboard/', views.dashboard, name='dashboard'),
+    path('shop/', views.shop, name='shop'),
 ]
