18781888cc37c7c7be68523a2403ae35d40f3afe
Claire Lally <clairelally08@gmail.com>
2020-06-11 18:43:36 +0000

    URL for about page

--- main/urls.py
+++ main/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('shop/', views.shop, name='shop'),
 ]
