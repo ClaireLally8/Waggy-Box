d267929de7d27abda8f310149ecd9911f67a0f00
Claire Lally <clairelally08@gmail.com>
2020-06-12 17:39:10 +0000

    urls for contact page

--- main/urls.py
+++ main/urls.py
@@ -6,6 +6,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('shop/', views.shop, name='shop'),
 ]
