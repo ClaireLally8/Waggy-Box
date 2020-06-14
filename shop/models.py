7ff84ceb77242a34e07972e661e990e9c3bfa651
Claire Lally <clairelally08@gmail.com>
2020-06-14 12:40:30 +0000

    Created category model

--- shop/models.py
+++ shop/models.py
@@ -1,1 +1,11 @@
 from django.db import models
+
+
+class Category(models.Model):
+    class Meta:
+        verbose_name_plural = 'Categories'
+        
+    name = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.name
