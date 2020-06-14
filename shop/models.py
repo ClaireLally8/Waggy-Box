ad8a8a269ccbe9e3737da6003be4636cf49f0758
Claire Lally <clairelally08@gmail.com>
2020-06-14 12:41:27 +0000

    Created future items model

--- shop/models.py
+++ shop/models.py
@@ -23,3 +23,13 @@ class CurrentItem(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class FutureItem(models.Model):
+    pid = models.CharField(max_length=50, null=True, blank=True)
+    name = models.CharField(max_length=150)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
