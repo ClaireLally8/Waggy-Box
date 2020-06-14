aa39cebc5bef204c8e8fb33257ed8bac7d0aed93
Claire Lally <clairelally08@gmail.com>
2020-06-14 12:42:02 +0000

    Created admin part for models

--- shop/admin.py
+++ shop/admin.py
@@ -1,1 +1,7 @@
 from django.contrib import admin
+from .models import CurrentItem, FutureItem, Category
+
+admin.site.register(CurrentItem)
+admin.site.register(FutureItem)
+admin.site.register(Category)
+© 2020 GitHub, Inc.
