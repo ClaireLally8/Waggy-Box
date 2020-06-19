b23e256bed958c33a83b1f0fcd93355b41082c15
Claire Lally <clairelally08@gmail.com>
2020-06-19 19:21:48 +0000

    fixed small issue with model

--- checkout/models.py
+++ checkout/models.py
@@ -61,8 +61,8 @@ class OrderLineItem(models.Model):
         Override the original save method to set the lineitem total
         and update the order total.
         """
-        self.lineitem_total = self.product.price * self.quantity
+        self.lineitem_total = self.item.price * self.quantity
         super().save(*args, **kwargs)
 
     def __str__(self):
-        return f'pid {self.product.pid} on order {self.order.order_number}'
+        return f'pid {self.item.pid} on order {self.order.order_number}'
