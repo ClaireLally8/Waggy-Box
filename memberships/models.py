0b3b00a85df036051559eaa8572f13f2745ab4a3
Claire Lally <clairelally08@gmail.com>
2020-06-20 23:30:00 +0000

    Removed slug from models

--- memberships/models.py
+++ memberships/models.py
@@ -1,7 +1,10 @@
+from django.db.models.signals import post_save
+
 from django.conf import settings
 from django.db import models
 
 import stripe
+stripe.api_key = settings.STRIPE_SECRET_KEY
 
 MEMBERSHIP_CHOICES = (
     ('Regular', 'reg'),
@@ -12,7 +15,6 @@ MEMBERSHIP_CHOICES = (
 
 # Membership type View for DB admin
 class Membership(models.Model):
-    slug = models.SlugField()
     membership_type = models.CharField(
         choices=MEMBERSHIP_CHOICES,
         default='Regular',
@@ -33,6 +35,23 @@ class UserMembership(models.Model):
 
     def __str__(self):
         return self.user.username
+
+def post_save_usermembership_create(sender, instance, created, *args, **kwargs):
+    user_membership, created = UserMembership.objects.get_or_create(
+        user=instance)
+
+
+    if user_membership.stripe_customer_id is None or user_membership.stripe_customer_id == '':
+        new_customer_id = stripe.Customer.create(email=instance.email)
+        free_membership = Membership.objects.get(membership_type='Free')
+        user_membership.stripe_customer_id = new_customer_id['id']
+        user_membership.membership = free_membership
+        user_membership.save()
+
+
+post_save.connect(post_save_usermembership_create,
+                  sender=settings.AUTH_USER_MODEL)
+
 
 
 class Subscription(models.Model):
