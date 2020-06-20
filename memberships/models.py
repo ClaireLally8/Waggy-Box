2533e1a29e9013eef30d42660a5533bbbbfe1223
Claire Lally <clairelally08@gmail.com>
2020-06-20 19:07:51 +0000

    Created models for memberships

--- memberships/models.py
+++ memberships/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+MEMBERSHIP_CHOICES = (
+    ('Regular', 'reg'),
+    ('Premium', 'prem'),
+)
+
+# Membership type View for DB admin
+class Membership(models.Model):
+    slug = models.SlugField()
+    membership_type = models.CharField(
+        choices=MEMBERSHIP_CHOICES,
+        default='Regular',
+        max_length=30)
+    price = models.IntegerField(default=15)
+    stripe_plan_id = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.membership_type
