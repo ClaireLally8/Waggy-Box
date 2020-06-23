fe848c369e82f9e6e2f7e3337ad150cc28a6785c
Claire Lally <clairelally08@gmail.com>
2020-06-23 22:01:04 +0000

    Created view for updating membership

--- memberships/urls.py
+++ memberships/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.membership_list, name='membership_list'),
     path('payment/', views.payments, name='payments'),
+    path('update-membership/<subscription_id>/', views.update_membership, name='update_membership'),
 ]
