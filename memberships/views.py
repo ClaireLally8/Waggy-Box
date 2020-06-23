476617e938ad5ff726953da3a84335699e410845
Claire Lally <clairelally08@gmail.com>
2020-06-23 22:01:39 +0000

    imported stripe & re-done the member_list view

--- memberships/views.py
+++ memberships/views.py
@@ -1,6 +1,11 @@
 from django.conf import settings
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponsePermanentRedirect
 from .models import Membership, UserMembership, Subscription
+
+import stripe
+
+stripe_public_key = settings.STRIPE_PUBLIC_KEY
+stripe_secret_key = settings.STRIPE_SECRET_KEY
 
 
 def get_user_membership(request):
@@ -31,22 +36,56 @@ def membership_list(request):
     memberships = Membership.objects.all()
     current_membership = get_user_membership(request)
     user_membership = str(current_membership.membership)
+    print('wtf')
     context = {
         'memberships': memberships,
         'user_membership': user_membership,
     }
 
+    if request.method == "POST":
+        selected_membership_type = request.POST.get('membership_type')
+        selected_membership = Membership.objects.get(membership_type=selected_membership_type)
+        request.session['selected_membership_type'] = selected_membership.membership_type
+        print(request.session['selected_membership_type'])
+        return render(request, 'memberships/payment.html', context)
+
     return render(request, 'memberships/membership_list.html', context)
 
 
 def payments(request):
-    if request.method == "POST":
-        current_membership = get_user_membership(request)
-        selected_membership_type = request.POST.get('membership_type')
-        selected_membership = Membership.objects.get(membership_type=selected_membership_type)
-        request.session['selected_membership_type'] = selected_membership.membership_type
+    user_membership = get_user_membership(request)
+    selected_membership = get_selected_membership(request)
+    selected_membership_type = request.POST.get('membership_type')
 
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+    if request.method == "POST":
+        subscription = stripe.Subscription.create(
+                customer=user_membership.stripe_customer_id,
+                items=[
+                    { "plan": selected_membership.stripe_plan_id },
+                ]
+            )
         context = {
-            'selected_membership': selected_membership
+            'subscription_id': subscription.id
         }
-        return render(request, 'memberships/payment.html', context)
+        return redirect(request, 'memberships/update-success.html', context)
+
+    context = {
+        'selected_membership': selected_membership
+    }
+    
+    return render(request, 'memberships/payment.html', context)
+
+def update_membership(request):
+    user_membership = get_user_membership(request)
+    selected_membership = get_selected_membership(request)
+    user_membership.membership = selected_membership
+    user_membership.save()
+
+    sub, created = Subscription.objects.get_or_create(user_membership=user_membership)
+    sub.stripe_subscription_id = subscription_id
+    sub.active = True
+    sub.save()
+
+    return render(request, 'memberships/update-success.html', context)
