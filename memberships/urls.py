8015a316ff85ed109189f2fbc12bcfee411eec4f
Claire Lally <clairelally08@gmail.com>
2020-06-24 22:32:08 +0000

    Created subscription overview page

--- memberships/urls.py
+++ memberships/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 urlpatterns = [
+    path('', views.subscription_overview, name='subscription_overview'),
     path('', views.membership_list, name='membership_list'),
     path('payment/', views.payments, name='payments')
 ]
