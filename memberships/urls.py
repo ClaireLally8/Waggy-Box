a75b9d5e06a289cbc472ebb7b185f6e0a68afd92
Claire Lally <clairelally08@gmail.com>
2020-06-25 17:37:52 +0000

    REmoved subscription overview page

--- memberships/urls.py
+++ memberships/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.subscription_overview, name='subscription_overview'),
     path('types/', views.membership_list, name='membership_list'),
     path('payment/', views.payments, name='payments')
 ]
