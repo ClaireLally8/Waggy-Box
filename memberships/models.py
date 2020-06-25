70757a82886ea2b15700124bf557759802ad5907
Claire Lally <clairelally08@gmail.com>
2020-06-25 22:41:15 +0000

    Changed country default

--- memberships/models.py
+++ memberships/models.py
@@ -37,7 +37,7 @@ class UserMembership(models.Model):
     full_name = models.CharField(max_length=50, null=True, blank=True)
     email = models.EmailField(max_length=254, null=True, blank=True)
     phone_number = models.CharField(max_length=20, null=True, blank=True)
-    country = CountryField(blank_label='Country *',  null=True, blank=True)
+    country = CountryField(blank_label='Country',  default="Ireland")
     postcode = models.CharField(max_length=20, null=True, blank=True)
     town_or_city = models.CharField(max_length=40, null=True, blank=True)
     street_address1 = models.CharField(max_length=80, null=True, blank=True)
