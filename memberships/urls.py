7e5e8eafa6a837b278036a0c94513f239e267f69
Claire Lally <clairelally08@gmail.com>
2020-06-26 19:51:37 +0000

    Created URLS for sub overview and cancellation

--- memberships/urls.py
+++ memberships/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 
 urlpatterns = [
+    path('', views.sub_overview, name='sub_overview'),
     path('types/', views.membership_list, name='membership_list'),
-    path('payment/', views.payments, name='payments')
+    path('payment/', views.payments, name='payments'),
+    path('cancel/', views.cancelSubscription, name='cancelSubscription'),
 ]
