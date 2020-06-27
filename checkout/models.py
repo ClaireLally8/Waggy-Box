c6cc77948df693addee13d09dd36fe4ad6aa123c
Claire Lally <clairelally08@gmail.com>
2020-06-27 18:46:31 +0000

    Refined models & assigned default values

--- checkout/models.py
+++ checkout/models.py
@@ -10,7 +10,6 @@ from shop.models import CurrentItem
 
 
 class Order(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     order_number = models.CharField(max_length=32, null=False, editable=False)
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
