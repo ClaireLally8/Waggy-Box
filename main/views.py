0ef2188a9f815a664bcd1548e09e3555e2567d95
Claire Lally <clairelally08@gmail.com>
2020-07-04 00:02:36 +0000

    Created limited view on dashboard for future items

--- main/views.py
+++ main/views.py
@@ -3,7 +3,25 @@ from django.core.mail import send_mail
 from shop.models import CurrentItem, FutureItem
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
+from memberships.models import UserMembership, Subscription
 # Create your views here.
+
+@login_required()
+def get_user_membership(request):
+    user_membership_qs = UserMembership.objects.filter(user=request.user)
+    if user_membership_qs.exists():
+        return user_membership_qs.first()
+    return None
+
+
+@login_required()
+def get_user_subscription(request):
+    user_subscription_qs = Subscription.objects.filter(
+        user_membership=get_user_membership(request))
+    if user_subscription_qs.exists():
+        user_subscription = user_subscription_qs.first()
+        return user_subscription
+    return None
 
 
 def index(request):
@@ -19,9 +37,11 @@ def about(request):
 
 @login_required()
 def dashboard(request):
+    subscription = get_user_subscription(request)
     items = FutureItem.objects.all()
     context = {
         'items': items,
+        'subscription': subscription,
     }
     return render(request, 'main/dashboard.html', context)
 
